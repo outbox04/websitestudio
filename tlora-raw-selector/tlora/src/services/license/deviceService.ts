1de7dd4292b10609a1f8aee40a130dd13706ca61
outbox04 <sentotran2001@gmail.com>
2026-07-08 00:45:24 +0700

    first commit

--- tlora-raw-selector/tlora/src/services/license/deviceService.ts
+++
@@ -1,6 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-import type { DeviceInfo } from "@/license/licenseTypes";
-
-export async function getDeviceInfo(): Promise<DeviceInfo> {
-  return invoke<DeviceInfo>("get_device_info");
-}
